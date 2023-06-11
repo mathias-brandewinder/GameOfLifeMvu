76be9e218378d68f2b590d9c9cd20cb2f2186f8e
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2023-06-11 11:14:17 -0700

    Strip un-necessary parts from template

--- Shell.fs
+++ Shell.fs
@@ -1,11 +1,7 @@
 namespace GameOfLifeMvu
 
-/// This is the main module of your application
-/// here you handle all of your child pages as well as their
-/// messages and their updates, useful to update multiple parts
-/// of your application, Please refer to the `view` function
-/// to see how to handle different kinds of "*child*" controls
 module Shell =
+
     open Elmish
     open Avalonia
     open Avalonia.Controls
@@ -16,53 +12,33 @@ module Shell =
     open Avalonia.FuncUI.Hosts
     open Avalonia.FuncUI.Elmish
 
-    type State =
-        /// store the child state in your main state
-        { aboutState: About.State; counterState: Counter.State;}
+    type State = {
+        Rows: int
+        Columns: int
+        }
 
     type Msg =
-        | AboutMsg of About.Msg
-        | CounterMsg of Counter.Msg
+        | NextGeneration
 
-    let init =
-        let aboutState, aboutCmd = About.init
-        let counterState = Counter.init
-        { aboutState = aboutState; counterState = counterState },
-        /// If your children controls don't emit any commands
-        /// in the init function, you can just return Cmd.none
-        /// otherwise, you can use a batch operation on all of them
-        /// you can add more init commands as you need
-        Cmd.batch [ aboutCmd ]
+    let init () =
+        {
+            Rows = 10
+            Columns = 10
+        },
+        Cmd.none
 
     let update (msg: Msg) (state: State): State * Cmd<_> =
         match msg with
-        | AboutMsg bpmsg ->
-            let aboutState, cmd =
-                About.update bpmsg state.aboutState
-            { state with aboutState = aboutState },
-            /// map the message to the kind of message 
-            /// your child control needs to handle
-            Cmd.map AboutMsg cmd
-        | CounterMsg countermsg ->
-            let counterMsg =
-                Counter.update countermsg state.counterState
-            { state with counterState = counterMsg },
-            /// map the message to the kind of message 
-            /// your child control needs to handle
-            Cmd.none
+        | NextGeneration -> state, Cmd.none
 
-    let view (state: State) (dispatch) =
-        DockPanel.create
-            [ DockPanel.children
-                [ TabControl.create
-                    [ TabControl.tabStripPlacement Dock.Top
-                      TabControl.viewItems
-                          [ TabItem.create
-                                [ TabItem.header "Counter Sample"
-                                  TabItem.content (Counter.view state.counterState (CounterMsg >> dispatch)) ]
-                            TabItem.create
-                                [ TabItem.header "About"
-                                  TabItem.content (About.view state.aboutState (AboutMsg >> dispatch)) ] ] ] ] ]
+    let view (state: State) (dispatch: Msg -> unit) =
+        DockPanel.create [
+            DockPanel.children [
+                TextBlock.create [
+                    TextBlock.text "TODO"
+                    ]
+                ]
+            ]
 
     /// This is the main window of your application
     /// you can do all sort of useful things here like setting heights and widths
@@ -80,6 +56,6 @@ module Shell =
             //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
             //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
 
-            Elmish.Program.mkProgram (fun () -> init) update view
+            Elmish.Program.mkProgram init update view
             |> Program.withHost this
             |> Program.run
